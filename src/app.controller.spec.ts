8c737731a93e45fa93824fe37ddc7dab438633f8
Yaminichauhan100 <yamini.chauhan@appinventiv.com>
2022-11-23 12:47:45 +0530

    changes in services

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,19 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppService } from './app.service'
+import { AppService } from './app.service';
 describe('AppService', () => {
-  let service: AppService;
+  let  userService: AppService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AppService],
     }).compile();
 
-    service = module.get<AppService>(AppService);
+    userService = module.get<AppService>(AppService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 });
-
-
