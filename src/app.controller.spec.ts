72b950ead050d0be10f9378d0eba33714b4e0e49
Yaminichauhan100 <yamini.chauhan@appinventiv.com>
2022-11-23 12:40:30 +0530

    changes in services

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,29 +16,4 @@ describe('AppService', () => {
   });
 });
 
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// describe('AppController', () => {
-//   let appController: AppController;
-//   let appService: AppService;
-
-//   beforeEach(async () => {
-//     const app: TestingModule = await Test.createTestingModule({
-//       controllers: [AppController],
-//       //providers: [AppService],
-//     }).compile();
-
-//     appController = app.get<AppController>(AppController);
-//    // appService= app.get<AppService>(AppService);
-//   });
-
-//   describe('root', () => {
-//     // it('should return "Hello World!"', async () => {
-//     //   const result = ''
-//     //   jest.spyOn(appService, 'getHello').mockImplementation(() => result);
-//     //   expect(await appController.getHello()).toBe(result);
-//     });
-//   });
 
